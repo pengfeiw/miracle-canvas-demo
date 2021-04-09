7d1ba0a671c43bd74ede57d0d82c8ebb7a03fa53
wangpengfei <2601339907@qq.com>
2021-04-09 17:35:37 +0800

    chore: 完善代码

--- src/miracle/mouse.ts
+++ src/miracle/mouse.ts
@@ -1,4 +1,5 @@
 import Entity from "./entity";
+import {Rectangle, Point} from "./graphic";
 
 export enum Operator {
     BoxSelect = 1,
@@ -10,6 +11,7 @@ class MiracleMouseControl {
     private canvas: HTMLCanvasElement; // entity所在的画布
     private dragging = false; // 是否正在拖拽
     private operator = Operator.BoxSelect; // 用户此时的操作类型
+    private dynaimcRect?: Rectangle;
 
     public constructor(entities: Entity[], canvas: HTMLCanvasElement) {
         this.entities = entities;
@@ -23,18 +25,52 @@ class MiracleMouseControl {
         this.canvas.addEventListener("mousemove", this.onMouseMove);
     }
 
-    private onMouseDown(event: MouseEvent) {
-        this.dragging = true;
-    }
+    private redraw() {
+        const ctx = this.canvas.getContext("2d");
+        if (ctx) {
+            ctx.clearRect(0, 0, this.canvas.clientWidth, this.canvas.clientHeight);
+            ctx.setLineDash([]);
+            this.entities.forEach((ent) => {
+                ent.draw(ctx);
+            });
 
-    private onMouseMove(event: MouseEvent) {
-        if (this.dragging) {
-            
+            // 绘制动态矩形
+            if (this.dynaimcRect) {
+                const dynaimcRect = this.dynaimcRect;
+                ctx.setLineDash([6]);
+                ctx.strokeStyle = "black";
+                ctx.beginPath();
+                ctx.strokeRect(dynaimcRect.location.x, dynaimcRect.location.y, dynaimcRect.width, dynaimcRect.height);
+            }
         }
     }
 
-    private onMouseUp(event: MouseEvent) {
+    private onMouseDown = (event: MouseEvent) => {
+        if (event.button === 0) {
+            this.dragging = true;
+            this.dynaimcRect = new Rectangle(new Point(event.offsetX, event.offsetY), 0, 0);
+        }
+    }
+
+    private onMouseMove = (event: MouseEvent) => {
+        if (this.dragging) {
+            const w = event.offsetX - this.dynaimcRect!.location.x;
+            const h = event.offsetY - this.dynaimcRect!.location.y;
+            this.dynaimcRect = new Rectangle(this.dynaimcRect!.location, w, h);
+            this.redraw();
+        }
+    }
+
+    private onMouseUp = (event: MouseEvent) => {
+        this.entities.forEach((ent) => {
+            ent.isActive = false;
+        });
+        const dragging = this.dragging; 
         this.dragging = false;
+        this.dynaimcRect = undefined;
+        if (dragging) {
+            this.redraw();
+        }
     }
 }
 
